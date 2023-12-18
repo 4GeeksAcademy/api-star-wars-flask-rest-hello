862dd94a537e2937399f0526d4d7c7d08df9a7d5
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-12-18 23:54:58 +0000

    Began endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Star_Systems, Factions, Planets, Species, Characters 
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = User.query.all()
+    serialized_people = [user.serialize() for user in people]
+    return({'msg': 'Here you go all the people currently in the database', 'result': serialized_people}), 200 
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planets.query.all()
+    serialized_all_planets = [Planets.serialize() for Planets in all_planets]
+    return({'msg': 'Here you go all the planets currently in the database', 'result': serialized_all_planets}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,27 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
             "is_active": self.is_active,
             # do not serialize the password, its a security breach
         }
 
-class StarSystems(db.Model):
+
+class Star_Systems(db.Model):
+    __tablename__ = 'star_systems'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    number_of_planets = db.Column(db.Integer)
-    galactic_coordinates = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    galactic_coordinates = db.Column(db.String(50))
 
     def __repr__(self):
         return '<Starystems %r>' % self.starsystems
@@ -32,67 +36,9 @@ class StarSystems(db.Model):
         return {
             "id":  self.id,
             "name": self.name,
-            "number_of_planets": self.number_of_planets,
             "galactic_coordiantes": self.galactic_coordinates
         }
 
-class Planets(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    population = db.Column(db.Integer, nullable=False)
-    terrain = db.Column(db.String, nullable=False)
-    climate = db.Column(db.String, nullable=False)
-
-    def __repr__(self):
-        return '<Planets %r>' % self.planets
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "population": self.population,
-            "terrain": self.terrain,
-            "climate": self.climate,
-        }
-
-class Species(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullabe=False)
-    classification = db.Column(db.String, unique=True, nullabe=False)
-    lifespan = db.Column(db.Integer)
-    language = db.Column(db.String)
-
-    def __repr__(self):
-        return '<Species %r>' % self.species
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "classification": self.classification,
-            "lifespan": self.lifespan,
-            "language": self.language,
-        }
-
-class Characters(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    height = db.Column(db.Integer)
-    weight = db.Column(db.Integer)
-    birthdate = db.Column(db.Date)
-
-    def __repr__(self):
-        return '<Characters %r>' % self.characters
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "height": self.height,
-            "weight": self.weight,
-            "birthdate": self.birthdate,
-        }
-    
 class Factions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
@@ -113,4 +59,69 @@ class Factions(db.Model):
             "capital": self.capital,
             "affiliation": self.affiliation,
         }
+    
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.String(100))
+    climate = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.planets
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain,
+            "climate": self.climate,
+        }
+
+class Species(db.Model):
+    __tablename__ = 'species'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    classification = db.Column(db.String(100), unique=True, nullable=False)
+    lifespan = db.Column(db.Integer)
+    language = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Species %r>' % self.species
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "classification": self.classification,
+            "lifespan": self.lifespan,
+            "language": self.language,
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    height = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    birthdate = db.Column(db.Date)
+    gender = db.Column(db.String)
+    occupation = db.Column(db.String(50))
+
+
+    def __repr__(self):
+        return '<Characters %r>' % self.characters
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "weight": self.weight,
+            "birthdate": self.birthdate,
+            "gender": self.gender,
+            "occupation": self.occupation
+        }
+    
     
